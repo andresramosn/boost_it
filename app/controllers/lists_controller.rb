89c647a15cfdf6894c6f391db6c4c7533b49f732
Mireia Palmero <mipalmero2a@gmail.com>
2019-12-10 16:18:29 +0100

    User can add a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,14 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.user = @user
+    @list.save
+    redirect_to list_path(@list)
   end
 
   def edit
@@ -18,5 +23,11 @@ class ListsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
