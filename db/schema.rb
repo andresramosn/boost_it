2fa871587af05ae8471f1e0e7a038d64f86acd8a
Mireia Palmero <mipalmero2a@gmail.com>
2019-12-11 15:09:06 +0100

    we can create a list and a tip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_162550) do
+ActiveRecord::Schema.define(version: 2019_12_11_120937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_162550) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_tips_on_user_id"
   end
 
