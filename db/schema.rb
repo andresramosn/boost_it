6c1c173763211ec02bef3e9ede03131c4de1e56d
Andres Ramos <andresramosn@hotmail.com>
2019-12-11 17:02:24 +0100

    Bugfix: fixing master branch issue

---
+++ db/schema.rb
@@ -0,0 +1,103 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_12_11_124105) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "list_tips", force: :cascade do |t|
+    t.bigint "tip_id"
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_list_tips_on_list_id"
+    t.index ["tip_id"], name: "index_list_tips_on_tip_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "property_lists", force: :cascade do |t|
+    t.bigint "property_id"
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_property_lists_on_list_id"
+    t.index ["property_id"], name: "index_property_lists_on_property_id"
+  end
+
+  create_table "shares", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_shares_on_list_id"
+    t.index ["user_id"], name: "index_shares_on_user_id"
+  end
+
+  create_table "tips", force: :cascade do |t|
+    t.integer "rating"
+    t.text "review"
+    t.string "category"
+    t.string "title"
+    t.string "address"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["user_id"], name: "index_tips_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "photo"
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "list_tips", "lists"
+  add_foreign_key "list_tips", "tips"
+  add_foreign_key "lists", "users"
+  add_foreign_key "properties", "users"
+  add_foreign_key "property_lists", "lists"
+  add_foreign_key "property_lists", "properties"
+  add_foreign_key "shares", "lists"
+  add_foreign_key "shares", "users"
+  add_foreign_key "tips", "users"
+end
