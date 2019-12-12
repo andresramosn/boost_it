5e5d4711fb3d36ab9a849acd296f2f3e1ce486b3
Mireia Palmero <mipalmero2a@gmail.com>
2019-12-12 10:46:08 +0100

    pushing last changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
-
-
   resources :lists, only: [:index, :new, :create, :show] do
     resources :tips, only: [:new, :create]
   end
