031ac570c4cb8963071075cbae6bbcc06bb77f67
Inaki Mediavilla <imediabar@gmail.com>
2019-12-13 12:07:16 +0100

    Add address autocomplete to add new tip form and fix issues with javascript

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'webpacker'
 gem 'devise'
 gem 'geocoder'
 gem 'devise_invitable', '~> 2.0.0'
+gem "algoliasearch-rails"
+
 
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.9.1'
--- app/javascript/plugins/toggle_map.js
+++ app/javascript/plugins/toggle_map.js
@@ -1,26 +1,24 @@
 
 const toggleMap = () => {
   const info = document.getElementById('show-info');
-  console.log(info)
   const location = document.getElementById('location-div');
-  console.log(location)
   const button = document.getElementById('toogle-map');
-  console.log(button)
 
 
-  button.addEventListener("click", (event) => {
-    console.log(event)
-    if (location.classList.contains("d-none")) {
-      button.innerHTML = "Show details"
-      location.classList.remove("d-none")
-      info.classList.add("d-none");
-      photo.classList.add("d-none");
-    } else {
-      button.innerHTML = "Show map"
-      location.classList.add("d-none")
-      info.classList.remove("d-none");
-    };
-  });
+  if (button) {
+    button.addEventListener("click", (event) => {
+      if (location.classList.contains("d-none")) {
+        button.innerHTML = "Show details"
+        location.classList.remove("d-none")
+        info.classList.add("d-none");
+        // photo.classList.add("d-none");
+      } else {
+        button.innerHTML = "Show map"
+        location.classList.add("d-none")
+        info.classList.remove("d-none");
+      };
+    });
+  };
 };
 
 export { toggleMap };
