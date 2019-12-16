ee549b2e510c3df769eae3521b3fde183e13ad8f
Mireia Palmero <mipalmero2a@gmail.com>
2019-12-16 15:22:15 +0100

    tabs added

---
+++ app/javascript/components/tabs.js
@@ -0,0 +1,30 @@
+const changeTabs = () => {
+
+  const user_lists = document.querySelector(".user-lists");
+  const shared_lists = document.querySelector(".shared-lists");
+
+  const input = document.getElementById('user-lists');
+    if (input) {
+      input.addEventListener('click', () => {
+        user_lists.style.display = "block"
+
+        shared_lists.style.display = "none"
+
+      });
+
+  };
+
+const shared = document.getElementById('shared-lists');
+    if (shared) {
+      shared.addEventListener('click', () => {
+        user_lists.style.display = "none"
+        shared_lists.style.display = "block"
+      });
+
+  };
+
+
+};
+
+
+export { changeTabs };
