ce87514c73d792d9584545e71d3cb260e9d1d0f1
Andres Ramos <andresramosn@hotmail.com>
2019-12-17 18:27:38 +0100

    multiple shares

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -31,12 +31,36 @@ class SharesController < ApplicationController
     share_list = List.find(params[:list_id])
     share_user_guest = User.find(params[:guest_user_id])
     Share.create(user_id: share_user_guest.id, list_id: share_list.id)
-    redirect_to guests_path
+    if params[:path] == "add"
+      redirect_to guests_show_path(guest_user_id: params[:guest_user_id])
+    else
+      redirect_to guests_path
+    end
   end
 
   def show
-    @lists = current_user.lists
     @guest = User.find(params[:guest_user_id])
+
+    @lists = current_user.lists
+    @shared_lists = Share.where(user_id: @guest.id).map(&:list_id).map do |list_id|
+      List.find(list_id)
+    end
+
+    @unshared_lists = []
+
+    @lists.each do |to_share_list|
+      if @shared_lists.exclude?(to_share_list)
+        @unshared_lists << to_share_list
+      end
+    end
+
+    @guest_lists = @shared_lists.length
+    @guest_tips = 0
+    @shared_lists.each do |list|
+      if list.user_id == current_user.id
+        @guest_tips += list.tips.length
+      end
+    end
   end
 
   private
